19d9dc568cfb65b59b0045ceda8a794b146efae8
Mykal Coleman <mycoleman.art@gmail.com>
2023-12-05 20:34:42 -0800

    Bloom working, background broken

--- main.js
+++ main.js
@@ -3,23 +3,50 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 		
 
 let camera, scene, renderer;
 
 // Camera
 camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.y = 2 * Math.tan( Math.PI / 6 );
-camera.position.z = 2;
+camera.position.y = 6 * Math.tan( Math.PI / 6 );
+camera.position.z = 6;
 
+// Scene 
 scene = new THREE.Scene();
 			
 renderer = new THREE.WebGLRenderer( { alpha: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.minDistance = 5;
+controls.maxDistance = 30;
+
+const renderScene = new RenderPass(scene, camera);
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+
+
+
+// Constants
+const bloomPass = new UnrealBloomPass(
+	new THREE.Vector2(window.innerWidth, window.innerHeight),
+	1.6,
+	0.1,
+	0.85
+);
+
+bloomPass.threshold = 1; 
+bloomPass.strength = .3; 
+bloomPass.radius = 0; 
+
+const outputPass = new OutputPass();
+composer.addPass(bloomPass); 
+composer.addPass(outputPass);
 
 // Plane
 const geometry = new THREE.CircleGeometry(35, 32);
@@ -31,10 +58,31 @@ scene.add(plane);
 
 // Box
 const box = new THREE.BoxGeometry(.5,.5,.5);
-const boxMaterial = new THREE.MeshStandardMaterial({ color: '#6AE3FF'})
+const boxMaterial = new THREE.MeshStandardMaterial({
+	color: '#6AE3FF',
+	emissive: '#6AE3FF',
+	emissiveIntensity: 1.75	,  
+})
 const shadowTester = new THREE.Mesh(box, boxMaterial);
 shadowTester.castShadow = true;
 scene.add(shadowTester);
+
+// Emmission Testing
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32); 
+const emissiveMaterial = new THREE.MeshStandardMaterial({
+  color: 0xffffff, 
+  emissive: 0xffff00, 
+  emissiveIntensity: .6,  
+});
+
+const sphereMesh = new THREE.Mesh(sphereGeometry, emissiveMaterial);
+sphereMesh.position.set(0, 3, 0); 
+//scene.add(sphereMesh); 
+
+// Object Emission
+const emissionLight = new THREE.PointLight('#6AE3FF', 10, 10); 
+scene.add(emissionLight); 
+emissionLight.castShadow = true;
 
 // Text
 const fontLoader = new FontLoader();
@@ -78,8 +126,8 @@ spotLight.penumbra = 0.1; // Softens the edge of the spotlight's light cone
 spotLight.decay = 1; // Intensity decay
 spotLight.distance = 200; // Maximum distance of the light
 
-scene.add(spotLight);
-scene.add(spotLight.target);
+// scene.add(spotLight);
+// scene.add(spotLight.target);
 
 
 // Point Light Red
@@ -91,7 +139,7 @@ pointLight.castShadow = true;
 pointLight.intensity = 20;
 pointLight.distance = 15;
 
-scene.add(pointLight);
+// scene.add(pointLight);
 
 // Point Light Green
 const pointLight2 = new THREE.PointLight();
@@ -102,7 +150,7 @@ pointLight2.castShadow = true;
 pointLight2.intensity = 20;
 pointLight2.distance = 15;
 
-scene.add(pointLight2);
+// scene.add(pointLight2);
 
 // Point Light blue
 const pointLight3 = new THREE.PointLight();
@@ -113,20 +161,22 @@ pointLight3.castShadow = true;
 pointLight3.intensity = 20;
 pointLight3.distance = 15;
 
-scene.add(pointLight3);
+// scene.add(pointLight3);
 
 // Shadows
-directionalLight.shadow.mapSize.width = 1024;
-directionalLight.shadow.mapSize.height = 1024;
-directionalLight.shadow.camera.near = 0.5;
-directionalLight.shadow.camera.far = 50;
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.mapSize.height = 1024;
+pointLight.shadow.camera.near = 0.5;
+pointLight.shadow.camera.far = 50;
+
+
 
 // Render Shadows
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 // Background
-renderer.setClearColor( 0x000000, 0 );
+	renderer.setClearColor( '#0A0020');
 
 // Fog 
 scene.fog = new THREE.Fog( 'rgb(10,0,32)', 10, 27 );
@@ -154,6 +204,7 @@ loader.load(
 	  pointLight.shadow.bias = -0.009;
 	  pointLight2.shadow.bias = -0.009;
 	  pointLight3.shadow.bias = -0.009;
+	  emissionLight.shadow.bias = -0.009;
   
 	  // Set shadow map properties
 	  renderer.shadowMap.enabled = true;
@@ -170,7 +221,7 @@ loader.load(
   let time = 0;
 
 // Render
-const animate = function () {
+function animate() {
 	requestAnimationFrame(animate);
 
 	time += 0.01;
@@ -184,10 +235,13 @@ const animate = function () {
   
 
 	controls.update(); 
+
+	emissionLight.position.copy(shadowTester.position); 
   
-	renderer.render(scene, camera);
+	composer.render(renderer);
+	// renderer.render(scene, camera);
+	
+
   };
-  
-// Start animation
-animate();
-		
+
+animate ();		
